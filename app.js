43b5f7dee4f73ea6d8d2fee869c8e5d7c6fc00c9
ayush-yubi <ayush.gupta@go-yubi.com>
2024-03-13 13:53:52 +0530

    liquibase init

---
+++ app.js
@@ -0,0 +1,31 @@
+const { Liquibase } = require('liquibase');
+
+const config = {
+    classpath: '/Users/ayush.gupta/Downloads/mysql-connector-j-8.3.0/mysql-connector-j-8.3.0.jar',
+    changeLogFile: './changelog.xml',
+    url: 'jdbc:mysql://localhost:3306/liquibase_db',
+    username: 'root',
+    password: 'new_password',
+    logLevel: 'DEBUG', 
+};
+
+const liquibase = new Liquibase(config);
+
+function handleError(error) {
+    console.error('Error:', error);
+}
+
+liquibase.validate()
+    .then(() => {
+        console.log('Pre-update validation successful. Proceeding with Liquibase update...');
+        return liquibase.update();
+    })
+    .then(() => {
+        console.log('Liquibase update command executed successfully.');
+    })
+    .catch(error => {
+        handleError('Error applying changes:', error);
+        liquibase.rollback({ count: 1 }) 
+            .then(() => console.log('Rollback successful.'))
+            .catch(rollbackError => handleError('Error during rollback:', rollbackError));
+    });
